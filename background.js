ed8d7803eb0cf4780f9a30544dd533a2d223393d
mehedi hasan <mhshohan04@gmail.com>
2025-06-08 16:09:35 +0600

    fixed error

--- background.js
+++ background.js
@@ -1,22 +1,28 @@
+chrome.runtime.onInstalled.addListener(() => {
+  console.log('Focus Bubble extension installed');
+});
+
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  if (request.type === "start-timer") {
-    chrome.alarms.create("focusAlarm", { delayInMinutes: 25 });
+  if (request.type === 'start-timer') {
+    chrome.alarms.create('focusAlarm', { delayInMinutes: 25 });
+
     chrome.notifications.create({
-      type: "basic",
-      iconUrl: "icons/icon.png",
-      title: "Focus started",
-      message: "25 minutes of focus time! Let's go 🚀"
+      type: 'basic',
+      iconUrl: 'icons/icon.png',
+      title: 'Focus started',
+      message: '25 minutes of focus time! 💪'
     });
   }
 });
 
+//  this part can throw the "undefined" error if alarms permission is missing
 chrome.alarms.onAlarm.addListener((alarm) => {
-  if (alarm.name === "focusAlarm") {
+  if (alarm.name === 'focusAlarm') {
     chrome.notifications.create({
-      type: "basic",
-      iconUrl: "icons/icon.png",
+      type: 'basic',
+      iconUrl: 'icons/icon.png',
       title: "Time's up!",
-      message: "Take a break! 🧘"
+      message: 'Take a break! ⏳'
     });
   }
 });
